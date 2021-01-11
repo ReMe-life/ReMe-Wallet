484ee82cceff94158484fecdf306dce0300832ad
bakasura980 <lyubo@limechain.tech>
2021-01-11 13:11:47 +0200

    Fix dashboard design

--- src/app/components/dashboard/renderers/dashboard.tsx
+++ src/app/components/dashboard/renderers/dashboard.tsx
@@ -15,18 +15,9 @@ export const DashboardRender = function (context: any) {
                 </p>
             </div>
 
-            <div className='tokens-wrapper'>
-                <p>
-                    <div>Your ReMC balance&nbsp;&nbsp;&nbsp;<strong>ReMC {context.state.tokensBalance.formatted}</strong></div>
-                    <ul className='token-details'>
-                        <li><span>SignUp bonus</span><strong>{context.state.earnedTokens.signup}</strong></li>
-                        <li><span>From referrals</span><strong>{context.state.earnedTokens.referral}</strong></li>
-                    </ul>
-                </p>
-                <p> Your ETH balance&nbsp;&nbsp;&nbsp;<strong>ETH {context.state.ethBalance.formatted}</strong></p>
-            </div>
             <dl className='referral-titles'>
-                <dt>Earn bonus ReMC tokens when you invite friends and family. Just copy and share the link below.</dt>
+                <dt>Earn bonus ReMC tokens when you invite friends and family.</dt>
+                <dt>Just copy and share the link below.</dt>
             </dl>
             <div className='input-wrapper'>
                 <input type='text' value={context.state.referralLink} />
@@ -42,6 +33,42 @@ export const DashboardRender = function (context: any) {
                 </span>
                 <span className='underline' onClick={() => window.open('#')}><strong>ReMe Referral Program</strong></span>
             </div>
+
+            <div className='wallet-title'>
+                <span className='message'>
+                    Your Wallet Balance
+                </span>
+                <span className="line"></span>
+            </div>
+
+            <div className='tokens-claimed-wrapper'>
+                <p className="one-flex-item">
+                    <div><strong>ReMC's claimed and available now</strong></div>
+                    {/* <div>Your ReMC balance&nbsp;&nbsp;&nbsp;<strong>ReMC {context.state.tokensBalance.formatted}</strong></div> */}
+                    <ul className='token-details'>
+                        {/* <li><span>SignUp bonus</span><strong>{context.state.earnedTokens.signup}</strong></li> */}
+                        <li><span>Available ReMCs</span><span className="tokens-total-amount">{context.state.earnedTokens.referral}</span></li>
+                    </ul>
+                </p>
+                <p className="one-flex-item">
+                    <div><strong>Your ETH balance&nbsp;&nbsp;&nbsp;</strong></div>
+                    <ul className='token-details'>
+                        <li><span>Available ETH</span><span className="tokens-total-amount">{context.state.ethBalance.formatted}</span></li>
+                    </ul>
+                </p>
+            </div>
+
+            <div className='tokens-for-claiming-wrapper'>
+                <p className="one-flex-item">
+                    <div><strong>ReMC's ready to be claimed:</strong></div>
+                    <ul className='token-details'>
+                        <li><span>SignUp bonus</span>{context.state.earnedTokens.signup}</li>
+                        <li><span>From referrals</span>{context.state.earnedTokens.referral}</li>
+                        <li><span></span><span className="tokens-total-amount">15.0000</span></li>
+                    </ul>
+                </p>
+            </div>
+
 
         </section>
     )
