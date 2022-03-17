f2aab1e920151ac2ad1bc77d8be66c6ab7a0fbd2
raoasifraza11 <raoasifraza1@hotmail.com>
2022-03-17 18:09:34 +0500

    rephrase words

--- src/app/components/dashboard/renderers/dashboard.tsx
+++ src/app/components/dashboard/renderers/dashboard.tsx
@@ -88,7 +88,7 @@ export const DashboardRender = function (context: any) {
                 <dl className='referral-titles'>
                     <dt>Earn bonus REME tokens when you invite friends and family.</dt>
                     <dt>Just copy and share the link below, in an email something like
-                        <a href={'https://healthconnected.agilecrm.com/forms/6713543266729984'} target={'_blank'} rel="noreferrer">this</a>.
+                        <a href={'https://healthconnected.agilecrm.com/forms/6713543266729984'} target={'_blank'} rel="noreferrer"> this </a>.
                     </dt>
                 </dl>
                 <div className='input-wrapper'>
