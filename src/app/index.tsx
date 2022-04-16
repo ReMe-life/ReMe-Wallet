0cdd1438800b4f2b605ade682ddb0cbc93a646d0
raoasifraza11 <raoasifraza1@hotmail.com>
2022-04-16 20:57:12 +0500

    Fix path

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import './index.scss'
-import 'reactjs-popup/dist/index.css';
+import 'node_modules/reactjs-popup/dist/index.css';
 import React, { Component, ReactNode } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ApplicationBackground from './assets/images/background.6e81d4b2.jpg'
